b360f4ed40fe85bec7b3cef09c5fdaa9ebbed511
Faisal <mfaisalhidayatullah@gmail.com>
2022-01-01 11:48:34 +0700

    update readme

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import { Helmet } from 'react-helmet';
 
 function App() {
 	return (
-		<div className="App">
-			<Container>
-				<Header />
-				<Content />
-			</Container>
-			<Footer />
-		</div>
+		<>
+			<Helmet>
+				<title>Resto App</title>
+			</Helmet>
+			<div className="App">
+				<Container>
+					<Header />
+					<Content />
+				</Container>
+				<Footer />
+			</div>
+		</>
 	);
 }
 
